88ce7f045beea7285fa7b21479de7c5ef58b2b50
Tushar Marwaha <tushar17may@gmail.com>
2021-02-07 14:35:45 +0530

    Added Employee CRUD

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-const bodyparser = require("body-parser");// we are not using bodyparser
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
 
 const mongoose = require('mongoose');
-const { stringify } = require("querystring");
 mongoose.connect('mongodb://localhost/contactdance', {useNewUrlParser: true});
 const port = 8000;
 
-//Define mongoose schema
+//Define mongoose schema for user contact info
 const contactSchema = new mongoose.Schema({
     name: String,
     phone: String ,
@@ -18,6 +22,18 @@ const contactSchema = new mongoose.Schema({
   });
 
 const contact = mongoose.model('contact', contactSchema);
+
+//Define mongoose schema for employee info
+const employeeSchema = new mongoose.Schema({
+    name: String,
+    phone: String ,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+const employee = mongoose.model('employee', employeeSchema);
+//a new collection with name 'employees' is made in contactdance database
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'));//for serving static files using express
@@ -47,7 +63,12 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug',params);
 })
 
-//jaise hhi contact pe koi post request maarega tab main ek naya contact object banaunga ...main kahunga ki jo post req aa rhi hai usmein se content extract krke conatact object mein daal do
+app.get('/employee',(req,res)=>{
+    const params = { };
+    res.status(200).render('employee.pug',params);
+})
+
+// post request on /contact for new user
 app.post('/contact',(req,res)=>{
     var mydata = new contact(req.body);
     mydata.save().then(()=>{
@@ -55,11 +76,77 @@ app.post('/contact',(req,res)=>{
     }).catch(()=>{
             res.status(400).send("item was not saved to the database")
     });
-    
-    
-    // res.status(200).render('contact.pug');
 })
-// .catch() error ke liye hota hai agar error hoga to ye function run hoga
+
+// check if post request for insert or update
+app.post('/employee', (req, res) => {
+    if (req.body._id == undefined)
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+});
+
+//insert new employee
+function insertRecord(req, res) {
+    var mydata = new employee(req.body);
+    mydata.save().then(()=>{
+        // console.log("insert is running");
+        res.redirect('/employee/list');
+
+        // res.send("this item has been saved to the db")
+        }).catch(()=>{
+            res.status(400).send("item was not saved to the database")
+    });
+}
+
+// update employee 
+function updateRecord(req, res) {
+    employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) {
+            // console.log(req.body._id)
+            // console.log('update is running') 
+            res.redirect('/employee/list'); }
+        else {
+            console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+// to get all employees in list form
+app.get('/employee/list', (req, res) => {
+    employee.find((err, docs) => {
+        if (!err) {
+            // console.log(docs);
+            res.render("employeelist.pug", {
+                lists: docs
+            })}
+        else {
+            console.log('Error in retrieving employee list :' + err);
+        }
+    });
+});
+
+//to delete an employee
+app.get('/employee/delete/:id', (req, res) => {
+    employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else { console.log('Error in employee delete :' + err); }
+    });
+});
+
+//get request to update an employee information
+app.get('/employee/:id', (req, res) => {
+    employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employeeupd.pug", {
+               viewTitle: "Update Employee",
+               employee: doc
+            });
+        }
+    });
+});
 
 
 //START THE SERVER
@@ -68,13 +155,6 @@ app.listen(port,()=>{
 })
 
 
-
-//agar aap app.post use krna chahte hain, matlab express ki madad se post krna chahte hain,matlab post request maar kr data ko database mein store krna chahte hai to hmein ek module install krna pdega jo hai ----->
-
-// npm install body parser
-
-// uske baad top par likhna hoga 
-// const bodyparser = require("body-parser");
 
 
 
